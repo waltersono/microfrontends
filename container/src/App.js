46faf2e6819026d9d375a77c5199b489e7152e4e
Walter Sono <tec.waltersono@gmail.com>
2025-10-27 08:51:02 +0200

    removing spaces

--- container/src/App.js
+++ container/src/App.js
@@ -3,8 +3,6 @@ import {Switch, Route, BrowserRouter } from 'react-router-dom';
 import {StylesProvider} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 
-
-
 export default () => {
     return (
         <div>
